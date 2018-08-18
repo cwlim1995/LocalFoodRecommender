f0105c83fb6359d99d97629a12ce3d45605b9c79
cwlim <C140176@e.ntu.edu.sg>
2018-08-18 17:09:24 +0800

    added logging facility and .gitignore

--- src/main/java/BlogCrawler.java
+++ src/main/java/BlogCrawler.java
@@ -2,14 +2,19 @@ import java.io.IOException;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class BlogCrawler {
+    private static final Logger logger = LogManager.getLogger(BlogCrawler.class);
+
     public static void main(String[] args) throws IOException {
+        logger.trace("Entering application.");
         Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
         Elements newsHeadlines = doc.select("#mp-itn b a");
         
         for (Element headline: newsHeadlines) {
-            System.out.println(headline.attr("title") + headline.absUrl("href"));
+            logger.debug(headline.attr("title") + headline.absUrl("href"));
         }
     }
 }
